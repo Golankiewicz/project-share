1a641dd5ed9aed4f381d46a89bb0fa9b4c419a12
Golankiewicz <59702516+Golankiewicz@users.noreply.github.com>
2020-10-26 13:24:41 +0100

    Stylowanie headera

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -6,19 +6,30 @@ class HomeHeader extends Component {
     render() {
         return (
             <div class='home-header'>
+                <div class='hero-image'>
+                </div>
                 <div class='right'>
                     <div class='top-menu'>
                         <Login/>
                         <Menu/>
                     </div>
+                    <div class='intro'>
+                        <p>Zacznij pomagać! <br/>Oddaj niechciane rzeczy w zaufane ręce</p>
+                        <div class='decoration'></div>
+                        <div>
+                        <div class='rectangle'>ODDAJ RZECZY</div>
+                        <div class='rectangle'>ZORGANIZUJ ZBIÓRKĘ</div>
+                        </div>
+                    </div>
+
                    
                </div>
-               <div class='hero-image'>
-               <div class='intro'>
-                        <p>Zacznij pomagać! <br/>Oddaj niechciane rzeczy w zaufane ręce</p>
-                    </div>
+                   
+                   
                </div>
-            </div>
+        
+              
+         
         );
     }
 }
