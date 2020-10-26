2b4798669b49e1024b9ed47b38c030567f4a96e1
Golankiewicz <59702516+Golankiewicz@users.noreply.github.com>
2020-10-26 14:10:28 +0100

    Stylowanie threeColumns

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+    render() {
+        return (
+            <div class='display'>
+                10
+            </div>
+        );
+    }
+}
+
+export default Display;
--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+import Display from './Display'
+
 class HomeThreeColumns extends Component {
     render() {
         return (
-            <div>
-                HomeThreeColumns
+            <div class='home-three-columns'>
+                <div class='column'>
+                    <Display/>
+                    <h4>ODDANYCH WORKÓW</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+
+                </div>
+                <div class='column'>
+                <Display/>
+                    <h4>    WSPARTYCH ORGANIZACJI</h4>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+
+                </div>
+                <div class='column'>
+                <Display/>
+                    <h4>ZORGANIZOWANYCH ZBIÓREK</h4>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+
+                </div>
+                
             </div>
         );
     }
