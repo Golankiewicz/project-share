45b04574b90bd974aac8968d9e0d6345fd03a5c4
Golankiewicz <59702516+Golankiewicz@users.noreply.github.com>
2020-10-27 23:49:58 +0100

    stylowanie footer-contact

---
+++ src/components/FooterContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FooterContact extends Component {
+    render() {
+        return (
+            <div class='footer-contact'>
+               
+            </div>
+        );
+    }
+}
+
+export default FooterContact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import HomeHeader from './HomeHeader';
 import HomeThreeColumns from './HomeThreeColumns';
 import SimpleSteps from'./SimpleSteps';
 import AboutUs from'./AboutUs';
-import Footer from'./Footer';
+import FooterContact from'./FooterContact';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
@@ -21,7 +21,7 @@ class Home extends Component {
                <HomeThreeColumns/>
                <SimpleSteps/>
                <AboutUs/>
-               <Footer/>
+               <FooterContact/>
                 <Link to="/login">Go to login</Link>
             </div>
         );
